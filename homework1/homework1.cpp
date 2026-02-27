1dc92d695e216fc467775dc866adcb36e2c39e98
makkerzsombor <makker.zsombor@gmail.com>
2026-02-27 13:01:36 +0100

    Getters done

--- homework1/homework1.cpp
+++ homework1/homework1.cpp
@@ -9,7 +9,17 @@ public:
 
 	void toString() 
 	{
-		std::cout << "A tort szamalaloja: " << mNumerator << ", nevezoje: " << mDenomintator;
+		std::cout << "A tort szamalaloja: " << mNumerator << ", nevezoje: " << mDenomintator << std::endl;
+	}
+
+	int getNumerator() 
+	{
+		return mNumerator;
+	}
+
+	int getDenominator() 
+	{
+		return mDenomintator;
 	}
 
 private:	
@@ -20,15 +30,15 @@ private:
 
 int main()
 {   
-	int szamlalo;
+	/*int szamlalo;
 	int nevezo;
 	std::cout << "Adja meg a tort szamlalojat!";
 	std::cin >> szamlalo;
 	
 	std::cout << "Adja meg a tort nevezojet!";
-	std::cin >> nevezo;
+	std::cin >> nevezo;*/
 
-	Fragment tort1 = Fragment(szamlalo, nevezo);
+	Fragment tort1 = Fragment(2, 3);
 	tort1.toString();
-
+	std::cout << tort1.getNumerator() << " " << tort1.getDenominator() << std::endl;
 }
