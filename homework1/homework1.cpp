3eba6173b26a517854c70227d9ca27ea80f293e5
makkerzsombor <makker.zsombor@gmail.com>
2026-02-27 12:31:40 +0100

    Fragment class with basic toString() and constructor

--- homework1/homework1.cpp
+++ homework1/homework1.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
+class Fragment {
+public:
+	Fragment(const int numerator,const int denominator){
+		mNumerator = numerator;
+		mDenomintator = denominator;
+	}
+
+	void toString() 
+	{
+		std::cout << "A tort szamalaloja: " << mNumerator << ", nevezoje: " << mDenomintator;
+	}
+
+private:	
+	int mNumerator;
+	int mDenomintator;
+
+};
+
 int main()
-{
-    std::cout << "Hello World!\n"; //Test 
-	std::cout << "Hello World!\n"; //Benti
+{   
+	int szamlalo;
+	int nevezo;
+	std::cout << "Adja meg a tort szamlalojat!";
+	std::cin >> szamlalo;
+	
+	std::cout << "Adja meg a tort nevezojet!";
+	std::cin >> nevezo;
+
+	Fragment tort1 = Fragment(szamlalo, nevezo);
+	tort1.toString();
+
 }
